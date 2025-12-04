e8b3a7f936c879aa46ee84ea09ede863d35557fc
Aidan Grigg <aidangrigg02@gmail.com>
2025-12-04 17:26:01 +1100

    day3: p2

--- day3.c
+++ day3.c
@@ -43,12 +43,48 @@ int solveP1(char *filename) {
 }
 
 long long solveP2(char *filename) {
-  return 0l;
+  FILE *fptr = fopen(filename, "r");
+
+  if (fptr == NULL) {
+    printf("An error occured reading the file\n");
+    return 1;
+  }
+
+  char line[BUFFER_LEN];
+  long long ans = 0;
+
+  while (fgets(line, sizeof(line), fptr) != NULL) {
+    int len = strlen(line);
+
+    char ns[13];
+    ns[12] = '\0';
+    char *prev = line;
+
+    for (int i = 0; i < 12; i++) {
+      char *max = i == 0 ? prev : prev + 1;
+
+      for (char *curr = max; curr < (line + len) - 12 + i; curr++) {
+        if (*curr > *max) {
+          max = curr;
+        }
+      }
+
+      prev = max;
+      ns[i] = *max;
+    }
+
+    printf("%lld\n", atoll(ns));
+
+    ans += atoll(ns);
+  }
+
+  return ans;
 }
 
 int main() {
-  printf("Part 1: %d \n", solveP1("./inputs/day3.real"));
-  /* printf("Part 2: %lld\n", solveP2("./inputs/day3.real")); */
+  char *filename = "./inputs/day3.real";
+  printf("Part 1: %d \n", solveP1(filename));
+  printf("Part 2: %lld\n", solveP2(filename));
 
   return 0;
 }
